29c1a31e16308b93535fda27e8fdc6ca2e4a437a
CandyCasper <mmpaing0803@gmail.com>
2023-10-31 21:10:36 +0000

    changed static path in server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+var port = process.env.PORT || 5000;
+var router = express.Router();
+
+var app = express();
+
+app.use(serveStatic(__dirname + "/dist/static"));
+app.use(router);
+
+router.get('/*', function (req, res) {  
+     return res.sendFile(path.join(__dirname, '/dist', 'index.html'));
+});
+
+app.listen(port);
