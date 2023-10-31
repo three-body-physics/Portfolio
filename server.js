0841cdf697243b6cb45541d10f16459d8c279260
CandyCasper <mmpaing0803@gmail.com>
2023-10-31 22:17:56 +0000

    server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var router = express.Router();
 
 var app = express();
 
-app.use(serveStatic(path.join(__dirname, '/static')));
+app.use(serveStatic(path.join(__dirname, 'dist', 'static')));
 app.use(router);
 
 router.get('/*', function (req, res) {  
-     return res.sendFile(path.join(__dirname, 'index.html'));
+     return res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port);
