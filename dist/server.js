fbf1a967b7ce88c31e2f20819b3379eac9788dcb
CandyCasper <mmpaing0803@gmail.com>
2023-10-31 22:07:30 +0000

    config server.js

--- dist/server.js
+++ dist/server.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var path = require('path');
-var serveStatic = require('serve-static');
 var port = process.env.PORT || 5000;
+var serveStatic = require('serve-static')
 var router = express.Router();
 
 var app = express();
 
-app.use(serveStatic(__dirname + "/dist/static"));
+app.use(serveStatic(path.join(__dirname, '/static')));
 app.use(router);
 
 router.get('/*', function (req, res) {  
-     return res.sendFile(path.join(__dirname, '/dist', 'index.html'));
+     return res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port);
